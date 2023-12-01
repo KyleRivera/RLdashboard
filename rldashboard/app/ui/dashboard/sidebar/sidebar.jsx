d84b59f2065c5f0bd64c6140f050c2808ea5562f
KyleRivera97 <Kyle@flyairlift.com>
2023-12-01 18:07:12 -0500

    Sidebar

--- rldashboard/app/ui/dashboard/sidebar/sidebar.jsx
+++ rldashboard/app/ui/dashboard/sidebar/sidebar.jsx
@@ -1,3 +1,17 @@
+import styles from "./sidebar.module.css"
+import {
+    MdDashboard,
+    MdSupervisedUserCircle,
+    MdShoppingBag,
+    MdAttachMoney,
+    MdWork,
+    MdAnalytics,
+    MdPeople,
+    MdOutlineSettings,
+    MdHelpCenter,
+    MdLogout,
+} from "react-icons/md";
+
 const Sidebar = () => {
     return (
         <div>Sidebar</div>
