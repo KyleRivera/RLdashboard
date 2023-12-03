057a4b26f2e40be85b3b51408ad5b15efdcaf5a6
KyleRivera97 <Kyle@flyairlift.com>
2023-12-02 23:47:02 -0500

    Style Changes and formating

--- rldashboard/app/ui/dashboard/sidebar/menuLink/menuLink.jsx
+++ rldashboard/app/ui/dashboard/sidebar/menuLink/menuLink.jsx
@@ -1,12 +1,15 @@
-import styles from './menuLink.module.css'
-import Link from 'next/link'
-const MenuLink = ({item}) => {
-    return (
-        <link href={item.path} className={StyleSheet.container}>
-            {item.icon}
-            {item.title}
-        </link>
-    )
-}
+import styles from './menuLink.module.css';
+import Link from 'next/link';
 
-export default MenuLink
+const MenuLink = ({ item }) => {
+    return (
+        <Link href={item.path} className={styles.container}>
+            <span>
+                {item.icon}
+                {item.title}
+            </span>
+        </Link>
+    );
+};
+
+export default MenuLink;
--- rldashboard/app/ui/dashboard/sidebar/sidebar.jsx
+++ rldashboard/app/ui/dashboard/sidebar/sidebar.jsx
@@ -1,4 +1,4 @@
-
+import MenuLink from "app/ui/dashboard/sidebar/menuLink/menuLink"
 import styles from "./sidebar.module.css"
 import {
     MdDashboard,
@@ -66,12 +66,19 @@ const menuItems = [
 const Sidebar = () => {
     return (
         <div className={styles.container}>
-            <ul>
+            <div className={styles.user}>
+                <img className={styles.userImage} src="/noavatar.png" alt="" width="50" height="50"/>
+                <div className={styles.userDetail}>
+                    <span className={styles.username}>John Joe</span>
+                    <span className={styles.userTitle}>Administrator</span>
+                </div>
+            </div>
+            <ul className={styles.list}>
                 {menuItems.map((cat)=> (
                     <li key={cat.title}>
                      <span className={styles.cat}>{cat.title}</span>
                      {cat.list.map((item)=> (
-                        <MenuLink item={item.title}/>
+                        <MenuLink item={item} key={item.title}/>
                     ))}
                     </li>
                  ))}
